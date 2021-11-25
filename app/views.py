5aea8321b3b65da5e4f6fce5c46bc8f8ca9ab555
d0h <daosheng@gmail.com>
2021-11-25 17:40:34 +0800

    added new sliders for additional quality attributes

--- app/views.py
+++ app/views.py
@@ -34,22 +34,32 @@ def index():
         num_primary_schools = int(request.form.get("num_primary_schools_slider"))/100.0
         qlt_primary_schools = int(request.form.get("qlt_primary_schools_slider"))/100.0
         num_secondary_schools = int(request.form.get("num_secondary_schools_slider"))/100.0
+        qlt_secondary_schools = int(request.form.get("qlt_secondary_schools_slider"))/100.0
         num_hawker = int(request.form.get("num_hawker_slider"))/100.0
         qlt_hawker = int(request.form.get("qlt_hawker_slider"))/100.0
         num_eating_establishments = int(request.form.get("num_eating_establishments_slider"))/100.0
-        num_chas_clinics = int(request.form.get("num_chas_clinics_slider"))/100.0
+        num_clinics = int(request.form.get("num_clinic_slider"))/100.0
+        qlt_clinics = int(request.form.get("qlt_clinic_slider"))/100.0
         num_sports_facilities = int(request.form.get("num_sports_facilities_slider"))/100.0
+        qlt_sports_facilities = int(request.form.get("qlt_sports_facilities_slider"))/100.0
+        num_gym = int(request.form.get("num_gym_slider"))/100.0
+        qlt_gym = int(request.form.get("qlt_gym_slider"))/100.0
         num_community_centers = int(request.form.get("num_community_centers_slider"))/100.0
+        qlt_community_centers = int(request.form.get("qlt_community_centers_slider"))/100.0
         num_parks = int(request.form.get("num_parks_slider"))/100.0
+        qlt_parks = int(request.form.get("qlt_parks_slider"))/100.0
         num_malls = int(request.form.get("num_malls_slider"))/100.0
         qlt_malls = int(request.form.get("qlt_malls_slider"))/100.0
         num_supermarkets = int(request.form.get("num_supermarkets_slider"))/100.0
+        qlt_supermarkets = int(request.form.get("qlt_supermarkets_slider"))/100.0
         num_mrt = int(request.form.get("num_mrt_slider"))/100.0
         num_carparks = int(request.form.get("num_carparks_slider"))/100.0
         num_bus_stops = int(request.form.get("num_bus_stops_slider"))/100.0
         num_taxi_stands = int(request.form.get("num_taxi_stands_slider"))/100.0
 
-        print(minPrice,maxPrice,minArea,maxArea,districts,num_primary_schools,qlt_primary_schools,num_secondary_schools,num_hawker,qlt_hawker,num_eating_establishments,num_chas_clinics,num_sports_facilities,num_community_centers,num_parks,num_malls,qlt_malls,num_supermarkets,num_mrt,num_carparks,num_bus_stops,num_taxi_stands)
+        print(minPrice,maxPrice,minArea,maxArea,districts,num_primary_schools,qlt_primary_schools,num_secondary_schools,qlt_secondary_schools,num_hawker,qlt_hawker,
+        num_eating_establishments,num_clinics,qlt_clinics,num_sports_facilities,qlt_sports_facilities,num_gym,qlt_gym,num_community_centers,qlt_community_centers,
+        num_parks,qlt_parks,num_malls,qlt_malls,num_supermarkets,qlt_supermarkets,num_mrt,num_carparks,num_bus_stops,num_taxi_stands)
 
         #Handle edge case where there is only 1 district selected
         if len(districts) == 1:
@@ -68,7 +78,7 @@ def index():
                         + num_hawker_score * {num_hawker}
                         + qlt_hawker_score * {qlt_hawker}
                         + num_eating_establishments_score * {num_eating_establishments}
-                        + num_chas_clinics_score * {num_chas_clinics}
+                        + num_chas_clinics_score * {num_clinics}
                         + num_sports_facilities_score * {num_sports_facilities}
                         + num_community_centers_score * {num_community_centers}
                         + num_parks_score * {num_parks}
