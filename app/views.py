a205b64a88a3f02ea73a519696737531b523f166
ansellim <29158160+ansellim@users.noreply.github.com>
2021-11-26 01:30:56 +0800

    edit field names, harmonize with schema
    edited the field names so they are the same as the column names of the csv files i am producing (refer to readme for more info)

--- app/views.py
+++ app/views.py
@@ -32,34 +32,34 @@ def index():
                 districts.append(k[6:])
 
         num_primary_schools = int(request.form.get("num_primary_schools_slider"))/100.0
-        qlt_primary_schools = int(request.form.get("qlt_primary_schools_slider"))/100.0
+        quality_primary_schools = int(request.form.get("quality_primary_schools_slider"))/100.0
         num_secondary_schools = int(request.form.get("num_secondary_schools_slider"))/100.0
-        qlt_secondary_schools = int(request.form.get("qlt_secondary_schools_slider"))/100.0
+        quality_secondary_schools = int(request.form.get("quality_secondary_schools_slider"))/100.0
         num_hawker = int(request.form.get("num_hawker_slider"))/100.0
-        qlt_hawker = int(request.form.get("qlt_hawker_slider"))/100.0
+        quality_hawker = int(request.form.get("quality_hawker_slider"))/100.0
         num_eating_establishments = int(request.form.get("num_eating_establishments_slider"))/100.0
         num_clinics = int(request.form.get("num_clinic_slider"))/100.0
-        qlt_clinics = int(request.form.get("qlt_clinic_slider"))/100.0
+        quality_clinics = int(request.form.get("quality_clinic_slider"))/100.0
         num_sports_facilities = int(request.form.get("num_sports_facilities_slider"))/100.0
-        qlt_sports_facilities = int(request.form.get("qlt_sports_facilities_slider"))/100.0
+        quality_sports_facilities = int(request.form.get("quality_sports_facilities_slider"))/100.0
         num_gym = int(request.form.get("num_gym_slider"))/100.0
-        qlt_gym = int(request.form.get("qlt_gym_slider"))/100.0
+        quality_gym = int(request.form.get("quality_gym_slider"))/100.0
         num_community_centers = int(request.form.get("num_community_centers_slider"))/100.0
-        qlt_community_centers = int(request.form.get("qlt_community_centers_slider"))/100.0
+        quality_community_centers = int(request.form.get("quality_community_centers_slider"))/100.0
         num_parks = int(request.form.get("num_parks_slider"))/100.0
-        qlt_parks = int(request.form.get("qlt_parks_slider"))/100.0
+        quality_parks = int(request.form.get("quality_parks_slider"))/100.0
         num_malls = int(request.form.get("num_malls_slider"))/100.0
-        qlt_malls = int(request.form.get("qlt_malls_slider"))/100.0
+        quality_malls = int(request.form.get("quality_malls_slider"))/100.0
         num_supermarkets = int(request.form.get("num_supermarkets_slider"))/100.0
-        qlt_supermarkets = int(request.form.get("qlt_supermarkets_slider"))/100.0
+        quality_supermarkets = int(request.form.get("quality_supermarkets_slider"))/100.0
         num_mrt = int(request.form.get("num_mrt_slider"))/100.0
         num_carparks = int(request.form.get("num_carparks_slider"))/100.0
         num_bus_stops = int(request.form.get("num_bus_stops_slider"))/100.0
         num_taxi_stands = int(request.form.get("num_taxi_stands_slider"))/100.0
 
-        print(minPrice,maxPrice,minArea,maxArea,districts,num_primary_schools,qlt_primary_schools,num_secondary_schools,qlt_secondary_schools,num_hawker,qlt_hawker,
-        num_eating_establishments,num_clinics,qlt_clinics,num_sports_facilities,qlt_sports_facilities,num_gym,qlt_gym,num_community_centers,qlt_community_centers,
-        num_parks,qlt_parks,num_malls,qlt_malls,num_supermarkets,qlt_supermarkets,num_mrt,num_carparks,num_bus_stops,num_taxi_stands)
+        print(minPrice,maxPrice,minArea,maxArea,districts,num_primary_schools,quality_primary_schools,num_secondary_schools,quality_secondary_schools,num_hawker,quality_hawker,
+        num_eating_establishments,num_clinics,quality_clinics,num_sports_facilities,quality_sports_facilities,num_gym,quality_gym,num_community_centers,quality_community_centers,
+        num_parks,quality_parks,num_malls,quality_malls,num_supermarkets,quality_supermarkets,num_mrt,num_carparks,num_bus_stops,num_taxi_stands)
 
         #Handle edge case where there is only 1 district selected
         if len(districts) == 1:
@@ -72,31 +72,31 @@ def index():
         #Logic to query precomputed scores, compute overall score and return top 5 recommendations
         query = f"""
                  SELECT property_id, price, area, district,
-                        num_primary_schools_score * {num_primary_schools}
-                        + qlt_primary_schools_score * {qlt_primary_schools}
-                        + num_secondary_schools_score * {num_secondary_schools}
-                        -- + qlt_secondary_schools_score * {qlt_secondary_schools}
-                        + num_hawker_score * {num_hawker}
-                        + qlt_hawker_score * {qlt_hawker}
-                        + num_eating_establishments_score * {num_eating_establishments}
-                        + num_chas_clinics_score * {num_clinics}
-                        -- + qlt_chas_clinics_score * {qlt_clinics}
-                        + num_sports_facilities_score * {num_sports_facilities}
-                        -- + qlt_sports_facilities_score * {qlt_sports_facilities}
-                        -- + num_gym_score * {num_gym}
-                        -- + qlt_gym_score * {qlt_gym}
-                        + num_community_centers_score * {num_community_centers}
-                        -- + qlt_community_centers_score * {qlt_community_centers}
-                        + num_parks_score * {num_parks}
-                        -- + qlt_parks_score * {qlt_parks}
-                        + num_malls_score * {num_malls}
-                        + qlt_malls_score * {qlt_malls}
-                        + num_supermarkets_score * {num_supermarkets}
-                        -- + qlt_supermarkets_score * {qlt_supermarkets}
-                        + num_mrt_score * {num_mrt}
-                        + num_carparks_score * {num_carparks}
-                        + num_bus_stops_score * {num_bus_stops}
-                        + num_taxi_stands_score * {num_taxi_stands}
+                        num_primary_school * {num_primary_schools}
+                        + quality_primary_school * {quality_primary_schools}
+                        + num_secondary_school * {num_secondary_schools}
+                        + quality_secondary_school * {quality_secondary_schools}
+                        + num_hawker_center * {num_hawker}
+                        + quality_hawker_center * {quality_hawker}
+                        + num_eating_establishment * {num_eating_establishments}
+                        + num_clinic * {num_clinics}
+                        + quality_clinic * {quality_clinics}
+                        + num_other_public_sports_facility * {num_sports_facilities}
+                        + quality_other_public_sports_facility * {quality_sports_facilities}
+                        + num_gym * {num_gym}
+                        + quality_gym * {quality_gym}
+                        + num_community_center * {num_community_centers}
+                        + quality_community_center * {quality_community_centers}
+                        + num_park * {num_parks}
+                        + quality_park * {quality_parks}
+                        + num_mall * {num_malls}
+                        + quality_mall * {quality_malls}
+                        + num_supermarket * {num_supermarkets}
+                        + quality_supermarket * {quality_supermarkets}
+                        + num_mrt * {num_mrt}
+                        + num_carpark * {num_carparks}
+                        + num_bus_stop * {num_bus_stops}
+                        + num_taxi_stand * {num_taxi_stands}
                         as overall_score
                  FROM property
                  WHERE price>={minPrice} and price<={maxPrice} and area>={minArea} and area<={maxArea}
